01bb7c011acdedc5a4a68d3e773c34e0e5bb48d2
vinayrk77 <vinayrk26@gmail.com>
2025-07-20 08:58:37 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/ex_02_Java_Basics_part2/variables_01.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics_part2;
+
+public class variables_01 {
+    public static void main(String[] args) {
+        // data_types variable_name = variable_value //
+        byte age = 70;
+        age = 80;
+        System.out.println(age);
+    }
+}
